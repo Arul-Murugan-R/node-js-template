aa96a3f5202b5c86c73751282ea7636e44d1c3a3
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-08-31 13:27:12 +0530

    login using google documentation

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ app = express()
 
 
 app.set('view engine','ejs')
+// app.use('/login', (req, res, next) => {
+//     res.render('login',{title:'login page'})
+// })
 app.use('/', (req, res, next) => {
     res.render('index')
 })
-
 app.listen(5000, () => {
     console.log('http://localhost:5000/')
 })
